1cc6f7c8dd953b97e0359eb76c4b9584f938e224
shambhavits16 <shambhavits16@gmail.com>
2022-12-01 19:01:56 +0530

    task3

--- ToDo App/firebase.js
+++ ToDo App/firebase.js
@@ -1,14 +1,14 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyByDKUYzGF0N5yt5XcQ-EU1-eS8b9Xprw4",
-    authDomain: "oib-todo-app.firebaseapp.com",
-    databaseURL: "https://oib-todo-app-default-rtdb.firebaseio.com",
-    projectId: "oib-todo-app",
-    storageBucket: "oib-todo-app.appspot.com",
-    messagingSenderId: "642622619435",
-    appId: "1:642622619435:web:02af0fa690a2ad13754f6e",
-    measurementId: "G-3JMEQVKLYM"
-};
+    apiKey: "AIzaSyDV8p8n4ONyP77Qa8Hl0pLpFiyCpwSTGvs",
+    authDomain: "basic-to-do-webapp.firebaseapp.com",
+    projectId: "basic-to-do-webapp",
+    storageBucket: "basic-to-do-webapp.appspot.com",
+    messagingSenderId: "443551962156",
+    appId: "1:443551962156:web:c78496bf3b14f125cd6d89",
+    measurementId: "G-H4RZLQ6V09"
+  };
+
 
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
